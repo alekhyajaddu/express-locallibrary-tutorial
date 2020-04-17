b111252af35e2fb7498cd9e2e6574fbd384a5c08
alekhyajaddu <alekhyajaddu@gmail.com>
2020-04-17 13:51:33 -0500

    made changes

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const dotenv = require('dotenv')
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+let catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 
-const compression = require('compression');
-const helmet = require('helmet');
 
 const app = express();
 dotenv.config({ path: '.env' })
@@ -31,8 +29,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(helmet());	
-app.use(compression());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
