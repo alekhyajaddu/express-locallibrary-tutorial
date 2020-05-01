1ecd26db79da1cea7d1fb4cc0a6a069c4cc6ecae
alekhyajaddu <alekhyajaddu@gmail.com>
2020-05-01 13:43:40 -0500

    modified code

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -31,10 +31,19 @@ exports.author_delete_post = function(req, res) {
 };
 
 // Display Author update form on GET.
-exports.author_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update GET');
-};
+exports.author_update_get = function(req, res, next) {   
+    
+    req.sanitize('id').escape().trim();
+    Author.findById(req.params.id, function(err, author) {
+        if (err) {
+            debug('update error:' + err);
+            return next(err);
+        }
+        //On success
+        res.render('author_form', { title: 'Update Author', author: author });
+    });
 
+};
 // Handle Author update on POST.
 exports.author_update_post = function(req, res) {
     res.send('NOT IMPLEMENTED: Author update POST');
